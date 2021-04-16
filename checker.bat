d81937abb3c57d6903cb8260780bd0de22d9b5ed
Nakash Kumar <nakashkumar@gmail.com>
2021-04-16 14:49:27 +0500

    consistent styling

--- checker.bat
+++ checker.bat
@@ -1,4 +1,4 @@
-@echo off
+@ECHO OFF
 
 SETLOCAL ENABLEDELAYEDEXPANSION
 SET CC=gcc
@@ -6,25 +6,22 @@ SET CC=gcc
 SET totalTests=0
 SET totalPassed=0
 
-!CC! -v >nul 2>&1
-if %ERRORLEVEL% GEQ 1 (
-  echo !CC! is not configured.
-  echo Possible reasons for this error:
-  echo - C/C++ compiler is not installed.
-  echo - Environment variable PATH is not set.
+!CC! -v >NUL 2>&1
+IF %ERRORLEVEL% GEQ 1 (
+  ECHO !CC! is not configured.
   EXIT /B %ERRORLEVEL%
-) else (
-  if not exist tmp (mkdir tmp)
-  for /r tests %%i in (*.in) DO (
+) ELSE (
+  IF not EXIST tmp (mkdir tmp)
+  FOR /r tests %%i in (*.in) DO (
     CALL :RunTest %%%i
   )
-  echo.
-  echo Total Tests !totalTests!
-  echo Tests Passed !totalPassed!
-  echo.
-  @del /S /Q tmp >NUL
-  @rmdir tmp >NUL
-  pause
+  ECHO.
+  ECHO Total Tests !totalTests!
+  ECHO Tests Passed !totalPassed!
+  ECHO.
+  DEL /S /Q tmp >NUL
+  RMDIR tmp >NUL
+  PAUSE
   EXIT /B %ERRORLEVEL%
 )
 
@@ -32,51 +29,51 @@ if %ERRORLEVEL% GEQ 1 (
     CALL :GetToken %~1
     SET /a totalTests+=1
     !CC! src\!qName!.c -o tmp\main >log.txt 2>&1
-    if %ERRORLEVEL% EQU 0 (
+    IF %ERRORLEVEL% EQU 0 (
       .\tmp\main.exe < tests\!tType!\!qName!\!tFile! > .\tmp\output
-      if %ERRORLEVEL% EQU 0 (
+      IF %ERRORLEVEL% EQU 0 (
         CALL :CheckOutput
-      ) else (
+      ) ELSE (
         CALL :PrintStatus "Failed (Runtime Error)" 
       )
-    ) else (
+    ) ELSE (
         CALL :PrintStatus "Failed (Compilation Error)"
     )
   ENDLOCAL
 EXIT /B 0
 
 :GetToken
-  set string=%~1
+  SET string=%~1
   SET "tType="
   SET "qName="
   SET "tFile="
   SET "tName="
-  for %%a in (%string:\= %) do (
-    set tType=!qName!
-    set qName=!tFile!
-    set tFile=%%a
+  FOR %%a in (%string:\= %) DO (
+    SET tType=!qName!
+    SET qName=!tFile!
+    SET tFile=%%a
   )
-  for /F "tokens=1 delims=." %%a in ("!tFile!") DO SET tName=%%a
+  FOR /F "tokens=1 delims=." %%a in ("!tFile!") DO SET tName=%%a
 EXIT /B 0
 
 :ReadFile
-  set "contents="
-  for /f "delims=" %%i in ('type %~1') DO SET contents=!contents! %%i
+  SET "contents="
+  FOR /f "delims=" %%i in ('type %~1') DO SET contents=!contents! %%i
 EXIT /B 0
 
 :PrintStatus
-  echo [!tType!] !qName! test#!tName!     %~1
+  ECHO [!tType!] !qName! test#!tName!     %~1
 EXIT /B 0
 
 :CheckOutput
   CALL :ReadFile "tmp\output"
-  set studentOutput=!contents!
+  SET studentOutput=!contents!
   CALL :ReadFile ".\tests\!tType!\!qName!\!tName!.out"
-  set judgeOutput=!contents!
-  if !studentOutput! == !judgeOutput! (
+  SET judgeOutput=!contents!
+  IF !studentOutput! == !judgeOutput! (
     CALL :PrintStatus "Passed :)"
     SET /a totalPassed+=1
-  ) else (
+  ) ELSE (
     CALL :PrintStatus "Failed :("
   )
 EXIT /B 0
